f2761413f99eb4ea859c2e4c0c2aa47a1f9174d3
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-06-09 17:22:29 +0900

    Create light CLI app

---
+++ PracticeCli/Program.cs
@@ -0,0 +1,19 @@
+﻿
+class Program
+{
+  static void Main(string[] args)
+  {
+    do
+    {
+      Console.Write("\nInput your filepath: ");
+      string? filepath = Console.ReadLine(); // "/Users/ryohei/Documents/Blank.docx";
+      if (string.IsNullOrEmpty(filepath)) break;
+      PracticeLibrary.Class1.OpenWordprocessingDocumentReadonly(filepath);
+      Console.WriteLine("\nOne Loop Done!");
+
+    } while (true);
+    Console.WriteLine("\nFinished!");
+
+    return;
+  }
+}
